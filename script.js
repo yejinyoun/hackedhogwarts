7344167dc9e5b80db564010df750ee7d757e3ff2
yejinyoun <yejinyoun@icloud.com>
2023-03-06 20:35:57 +0100

    global array and prototype set

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+window.addEventListener("DOMContentLoaded", start);
+
+let allStudents = [];
+
+const Student = {
+  firstName: "-default-",
+  middleName: undefined,
+  nickName: undefined,
+  lastName: "-default-",
+  gender: "-default-",
+  photo: "-default-",
+  house: "-default-",
+  blood: "-default-",
+  prefect: false,
+  squad: false,
+  expelled: false,
+};
+
+function start() {
+  console.log("start");
+}
