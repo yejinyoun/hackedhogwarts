a5a255ef4322fd06b3baf45918e863cd96701340
yejinyoun <yejinyoun@icloud.com>
2023-03-06 21:30:20 +0100

    data cleaned

--- script.js
+++ script.js
@@ -20,4 +20,55 @@ const Student = {
 
 function start() {
   console.log("start");
+  loadJSON();
 }
+
+function loadJSON() {
+  fetch("https://petlatkea.dk/2021/hogwarts/students.json")
+    .then((response) => response.json())
+    .then((jsonData) => {
+      // when loaded, prepare objects
+      cleanObjects(jsonData);
+    });
+}
+
+function cleanObjects(objects) {
+  allStudents = objects.map(cleanObject);
+}
+
+function cleanObject(object) {
+  object["fullname"] = capitalize(object["fullname"].trim());
+  object["gender"] = capitalize(object["gender"].trim());
+  object["house"] = capitalize(object["house"].trim());
+
+  function capitalize(string) {
+    let capitalized = string[0].toUpperCase();
+
+    for (let i = 1; i < string.length; i++) {
+      if (string[i - 1] === " " || string[i - 1] === "-") {
+        capitalized += string[i].toUpperCase();
+      } else {
+        capitalized += string[i].toLowerCase();
+      }
+    }
+    return capitalized;
+  }
+  return object;
+}
+
+/* function prepareObjects(objects) {
+  allStudents = objects.map(prepareObject);
+}
+
+function prepareObject(object) {
+  const student = Object.create(Student);
+
+  const fullname = object["fullname"].trim();
+  animal.name = texts[0];
+  animal.desc = texts[2];
+  animal.type = texts[3];
+  animal.age = jsonObject.age;
+
+  return animal;
+}
+ */
